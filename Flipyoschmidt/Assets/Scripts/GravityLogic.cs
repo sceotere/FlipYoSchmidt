e1b899bb3543d2912a6df4c5d797ce165e08a322
Jake Gianola <jakegianola204@gmail.com>
2020-04-28 13:59:55 -0700

    Implemeted rudimentary gravity switching by pressing space

---
+++ Flipyoschmidt/Assets/Scripts/GravityLogic.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GravityLogic : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called at least once per frame depending on physics engine needs
+    void FixedUpdate()
+    {
+        
+    }
+}
---
+++ Flipyoschmidt/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public CharacterController controller;
+    public float gravity = 2.0f;
+
+    public float speed = 12f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+
+        controller.Move(move * speed * Time.deltaTime);
+
+        //If the player presses space, switch gravity
+        if(Input.GetKeyDown(KeyCode.Space)){
+            gravity = -1 * gravity;
+        }
+    }
+
+    // Update is called at least once per frame
+    void FixedUpdate()
+    {
+        //Account for gravity
+        Vector3 grav = (-transform.up * gravity);
+        controller.Move(grav * speed * Time.deltaTime);
+    }
+}
