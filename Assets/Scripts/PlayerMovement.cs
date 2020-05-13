c2ebfc6ebb9b906469d98510aac7e4fbf0b208ff
Joseph Goh <jgoh2@uoregon.edu>
2020-05-13 00:13:06 -0700

    Removed redundant isFlipped variable and edited to use orientation variable directly in most cases

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,4 +1,15 @@
-﻿using System.Collections;
+﻿/*
+ * Flip Yo' Schmidt
+ * 
+ * PlayerMovement.cs
+ * Description: Script that controls player movement, gravity, and the first person camera
+ * 
+ * Author: Jake Gianola, Joseph Goh
+ * Acknowledgements: N/A
+ * Last Updated: 05/12/2020
+ */
+
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -13,8 +24,6 @@ public class PlayerMovement : MonoBehaviour
     bool isGrounded = true; // Our own isGrounded variable since we can be on the ceiling too
     float y_vel = 0; // y-axis (vertical in a 3d sense) velocity for gravity
 
-    bool isflipping = false; //Bool to check if the player is upside down or right side up
-
     private void Start()
     {
         Physics.gravity = new Vector3(0.0f, -1 * gravity * 10f, 0.0f);
@@ -28,6 +37,54 @@ public class PlayerMovement : MonoBehaviour
         Application.Quit();
         }
 
+        // Set the isGrounded variable appropriately
+        SetGrounded();
+
+        // If the player is grounded, y_vel should be a slight groundward force
+        // This helps isGrounded be consistently updated.
+        if (isGrounded)
+        {
+            y_vel = -0.01f * orientation;
+        }
+
+        //If the player presses space, try to switch gravity
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            FlipGravity();
+        }
+
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+        Vector3 move;
+
+        // Multiply x by orientation to match camera left/right
+        move = transform.right * orientation * x + transform.forward * z;
+
+        // If the player is not currently grounded, adjust y_vel for gravity
+        if (!isGrounded)
+        {
+            y_vel -= orientation * gravity * Time.deltaTime;
+        }
+        // Set move vector's y 
+        move.y = y_vel;
+
+        controller.Move(move * speed * Time.deltaTime);
+    }
+
+    void FlipGravity()
+    {
+        //If the player is grounded, then switch gravity orientation and reset isGrounded
+        if (isGrounded)
+        {
+            orientation *= -1;
+            // Not sure what gravity multiplier best syncs player acceleration with normal rigidbody acceleration
+            Physics.gravity = new Vector3(0.0f, -1 * orientation * gravity * 10f, 0.0f);
+            isGrounded = false;
+        }
+    }
+
+    void SetGrounded()
+    {
         // Set isGrounded value based on current orientation
         if (orientation == 1 && (controller.collisionFlags & CollisionFlags.Below) != 0)
         {
@@ -43,61 +100,5 @@ public class PlayerMovement : MonoBehaviour
         {
             isGrounded = false;
         }
-
-        // If the player is grounded, y_vel should be a slight groundward force
-        // This helps isGrounded be consistently updated.
-        if (isGrounded)
-        {
-            y_vel = -0.01f * orientation;
-        }
-
-        //If the player presses space and we are grounded, then switch gravity orientation and reset isGrounded
-        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
-        {
-            orientation *= -1;
-            isflipping = !isflipping;
-            // Not sure what gravity multiplier best syncs player acceleration with normal rigidbody acceleration
-            Physics.gravity = new Vector3(0.0f, -1 * orientation * gravity * 10f, 0.0f);
-            isGrounded = false;
-        }
-
-        float x = Input.GetAxis("Horizontal");
-        float z = Input.GetAxis("Vertical");
-        Vector3 move;
-
-        if(orientation < 0)
-        {
-            move = transform.right * -1 * x + transform.forward * z;
-        }
-
-        else
-        {
-            move = transform.right * x + transform.forward * z;
-        }
-
-
-        // If the player is not currently grounded, adjust y_vel for gravity
-        if (!isGrounded)
-        {
-            y_vel -= orientation * gravity * Time.deltaTime;
-        }
-        // Set move vector's y 
-        move.y = y_vel;
-
-        controller.Move(move * speed * Time.deltaTime);
-
-        /*Flip camera if gravity is reversed.
-        if(orientation < 0 && isflipping == true)
-        {
-            transform.rotation = Quaternion.Euler(180, 0, 0);
-            isflipping = false;
-        }
-
-        //Flip camera back if gravity is normal.
-        else if (orientation > 0 && isflipping == false)
-        {
-            transform.rotation = Quaternion.Euler(0, 0, 0);
-            isflipping = true;
-        } */
     }
 }
